497ae0611180d0861c0beb516bb71c966c055d2a
tedwatters <ted.watters@gmail.com>
2020-11-17 11:24:03 -0500

    modified:   cnn.py 	new file:   cnn_cifar_10.png 	new file:   cnn_cifar_10_beta1.png 	deleted:    img/Rnn.png 	deleted:    img/graph.png 	deleted:    img/nas.jpeg 	deleted:    img/swish_.png 	deleted:    img/swish_com.png 	deleted:    img/swish_graph.png 	deleted:    main/HEAD 	deleted:    main/config 	deleted:    main/description 	deleted:    main/hooks/applypatch-msg.sample 	deleted:    main/hooks/commit-msg.sample 	deleted:    main/hooks/fsmonitor-watchman.sample 	deleted:    main/hooks/post-update.sample 	deleted:    main/hooks/pre-applypatch.sample 	deleted:    main/hooks/pre-commit.sample 	deleted:    main/hooks/pre-push.sample 	deleted:    main/hooks/pre-rebase.sample 	deleted:    main/hooks/pre-receive.sample 	deleted:    main/hooks/prepare-commit-msg.sample 	deleted:    main/hooks/update.sample 	deleted:    main/info/exclude 	new file:   requirements.txt 	deleted:    src/.ipynb_checkpoints/Searching for activation function-checkpoint.ipynb 	deleted:    src/Searching for activation function.ipynb 	deleted:    src/WMT14.py 	deleted:    src/__pycache__/config.cpython-35.pyc 	deleted:    src/__pycache__/config.cpython-36.pyc 	deleted:    src/__pycache__/dataset.cpython-35.pyc 	deleted:    src/__pycache__/models.cpython-35.pyc 	deleted:    src/__pycache__/models.cpython-36.pyc 	deleted:    src/__pycache__/network.cpython-35.pyc 	deleted:    src/__pycache__/network.cpython-36.pyc 	deleted:    src/__pycache__/parser.cpython-35.pyc 	deleted:    src/__pycache__/parser.cpython-36.pyc 	deleted:    src/__pycache__/resnet.cpython-35.pyc 	deleted:    src/__pycache__/resnet.cpython-36.pyc 	deleted:    src/__pycache__/resnet_model.cpython-35.pyc 	deleted:    src/__pycache__/resnet_model.cpython-36.pyc 	deleted:    src/__pycache__/utils.cpython-35.pyc 	deleted:    src/__pycache__/utils.cpython-36.pyc 	deleted:    src/childnetwork.py 	deleted:    src/cifar100_download_and_extract.py 	deleted:    src/cifar100_test.py 	deleted:    src/cifar100_train.py 	deleted:    src/cifar10_download_and_extract.py 	deleted:    src/config.py 	deleted:    src/dataset.py 	deleted:    src/img/Rnn.png 	deleted:    src/img/activationfunctions.png 	deleted:    src/img/graph.png 	deleted:    src/img/loss_rmsprop.png 	deleted:    src/img/nas.jpeg 	deleted:    src/main.py 	deleted:    src/parser.py 	deleted:    src/rnn_controller.py 	deleted:    src/swish.py 	deleted:    src/train/events.out.tfevents.1514935452.6bf252a4b161 	deleted:    src/utils.py 	deleted:    untitled

--- cnn.py
+++ cnn.py
@@ -11,6 +11,56 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
+from collections.abc import Iterable
+
+import numpy as np
+
+
+
+'''
+Define Swish Function
+'''
+
+from tensorflow.keras.layers import Layer
+import tensorflow.keras.backend as K
+
+def swish(x, beta=1.0):
+    return x * K.sigmoid(beta * x)
+
+class Swish(Layer):
+
+    def __init__(self, beta=1.0, trainable=False,  **kwargs):
+        super(Swish, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.beta = beta
+        self.trainable = trainable
+
+    def build(self, input_shape):
+        self.beta_factor = K.variable(self.beta,
+                                      dtype=K.floatx(),
+                                      name='beta_factor')
+        if self.trainable:
+            self._trainable_weights.append(self.beta_factor)
+
+        super(Swish, self).build(input_shape)
+
+    def call(self, inputs, mask=None):
+        return swish(inputs, self.beta_factor)
+
+    def get_config(self):
+        config = {'beta': self.get_weights()[0] if self.trainable else self.beta,
+                  'trainable': self.trainable}
+        base_config = super(Swish, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+'''
+Get data
+'''
+
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Normalize pixel values to be between 0 and 1
@@ -31,38 +81,184 @@ for i in range(25):
     plt.xlabel(class_names[train_labels[i][0]])
 plt.show()
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+'''
+ReLU model
+'''
 
-model.summary()
+r_model = models.Sequential()
+r_model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+r_model.add(layers.Activation('relu'))
+r_model.add(layers.MaxPooling2D((2, 2)))
+r_model.add(layers.Conv2D(64, (3, 3)))
+r_model.add(layers.Activation('relu'))
+r_model.add(layers.MaxPooling2D((2, 2)))
+r_model.add(layers.Conv2D(64, (3, 3)))
+r_model.add(layers.Activation('relu'))
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
 
-model.summary()
+r_model.add(layers.Flatten())
+r_model.add(layers.Dense(64))
+r_model.add(layers.Activation('relu'))
 
-model.compile(optimizer='adam',
+r_model.add(layers.Dense(10))
+
+r_model.summary()
+
+r_model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
+r_history = r_model.fit(train_images, train_labels, epochs=5, 
                     validation_data=(test_images, test_labels))
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+'''
+Swish model
+'''
+
+s_model = models.Sequential()
+s_model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+s_model.add(Swish(beta=1.0, trainable=True,name='swish1'))
+s_model.add(layers.MaxPooling2D((2, 2)))
+s_model.add(layers.Conv2D(64, (3, 3)))
+s_model.add(Swish(beta=1.0, trainable=True,name='swish2'))
+s_model.add(layers.MaxPooling2D((2, 2)))
+s_model.add(layers.Conv2D(64, (3, 3)))
+s_model.add(Swish(beta=1.0, trainable=True,name='swish3'))
+
+
+
+s_model.add(layers.Flatten())
+s_model.add(layers.Dense(64))
+s_model.add(Swish(beta=1.0, trainable=True,name='swish4'))
+
+s_model.add(layers.Dense(10))
+
+s_model.summary()
+
+s_model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+s_history = s_model.fit(train_images, train_labels, epochs=5, 
+                    validation_data=(test_images, test_labels))
+
+'''
+Results
+'''
+
+plt.plot(r_history.history['accuracy'], label='relu accuracy')
+plt.plot(r_history.history['val_accuracy'], label = 'relu val_accuracy')
+plt.plot(s_history.history['accuracy'], label='swish accuracy')
+plt.plot(s_history.history['val_accuracy'], label = 'swish val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+plt.ylim([0, 1])
 plt.legend(loc='lower right')
+plt.figure(0)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+r_test_loss, r_test_acc = r_model.evaluate(test_images,  test_labels, verbose=2)
+s_test_loss, s_test_acc = s_model.evaluate(test_images,  test_labels, verbose=2)
 
-print(test_acc)
+plt.savefig('cnn_cifar_10.png', bbox_inches='tight')
+
+  
+
+print(r_test_acc)
+print(s_test_acc)
+
+'''
+Beta values
+'''
+
+swish1_beta = []
+swish2_beta = []
+swish3_beta = []
+swish4_beta = []
+
+swish1_preact = []
+swish2_preact = []
+swish3_preact = []
+swish4_preact = []
+
+n=range(5)
+for i in n:
+    #reinitialize model
+    s_model = models.Sequential()
+    s_model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+    s_model.add(Swish(beta=1.0, trainable=True,name='swish1'))
+    s_model.add(layers.MaxPooling2D((2, 2)))
+    s_model.add(layers.Conv2D(64, (3, 3)))
+    s_model.add(Swish(beta=1.0, trainable=True,name='swish2'))
+    s_model.add(layers.MaxPooling2D((2, 2)))
+    s_model.add(layers.Conv2D(64, (3, 3)))
+    s_model.add(Swish(beta=1.0, trainable=True,name='swish3'))
+    s_model.add(layers.Flatten())
+    s_model.add(layers.Dense(64))
+    s_model.add(Swish(beta=1.0, trainable=True,name='swish4'))
+    s_model.add(layers.Dense(10)) 
+    s_model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+    s_history = s_model.fit(train_images, train_labels, epochs=5, 
+                    validation_data=(test_images, test_labels))
+    
+    #append results of beta and preactivations
+    swish1_beta.append(s_model.get_layer(name = 'swish1').get_weights())
+    swish2_beta.append(s_model.get_layer(name = 'swish2').get_weights())
+    swish3_beta.append(s_model.get_layer(name = 'swish3').get_weights())
+    swish4_beta.append(s_model.get_layer(name = 'swish4').get_weights())
+    
+    swish1_preact.append(s_model.get_layer(index = 0).get_weights()[0].tolist())
+    swish2_preact.append(s_model.get_layer(index = 3).get_weights()[0].tolist())
+    swish3_preact.append(s_model.get_layer(index = 6).get_weights()[0].tolist())
+    swish4_preact.append(s_model.get_layer(index = 9).get_weights()[0].tolist())
+    
+    i += 1
+    print(i)
+    
+
+def flatten(l):
+    for el in l:
+        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
+            yield from flatten(el)
+        else:
+            yield el
+            
+bins = np.arange(0,3,0.1)
+            
+swish1_beta = list(flatten(swish1_beta))
+swish2_beta = list(flatten(swish2_beta))
+swish3_beta = list(flatten(swish3_beta))
+swish4_beta = list(flatten(swish4_beta))
+
+swish1_preact = list(flatten(swish1_preact))
+swish2_preact = list(flatten(swish2_preact))
+swish3_preact = list(flatten(swish3_preact))
+swish4_preact = list(flatten(swish4_preact))
+
+plt.hist(x=swish1_beta[0], bins=bins, alpha=0.7, rwidth=0.85)
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('Trained Betas - Swish Layer 1')
+maxfreq = n.max()
+# Set a clean upper y-axis limit.
+plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+plt.figure(1)
+
+plt.savefig('cnn_cifar_10_beta1.png', bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
