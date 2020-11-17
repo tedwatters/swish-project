93e096e370142de3fea707dbd9e732ec67e2db34
tedwatters <ted.watters@gmail.com>
2020-11-17 12:10:18 -0500

    modified:   cifar10_cnn.py

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -69,17 +69,6 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
 
 '''
 ReLU model
